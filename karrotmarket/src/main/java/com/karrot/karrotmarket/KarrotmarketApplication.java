29530217138a4aa19024b833047c9696ac4065c0
qkralswl689 <qkralswl689@gmail.com>
2022-01-08 23:00:50 +0900

    프로젝트생성

---
+++ karrotmarket/src/main/java/com/karrot/karrotmarket/KarrotmarketApplication.java
@@ -0,0 +1,13 @@
+package com.karrot.karrotmarket;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KarrotmarketApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KarrotmarketApplication.class, args);
+	}
+
+}
---
+++ karrotmarket/src/test/java/com/karrot/karrotmarket/KarrotmarketApplicationTests.java
@@ -0,0 +1,13 @@
+package com.karrot.karrotmarket;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KarrotmarketApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

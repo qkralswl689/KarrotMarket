b57f14c53fc885a4c21e3169663f8c9ad2e30330
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-01-09 12:21:05 +0900

    html 및 db연결

--- karrotmarket/src/main/java/com/karrot/karrotmarket/KarrotmarketApplication.java
+++ karrotmarket/src/main/java/com/karrot/karrotmarket/KarrotmarketApplication.java
@@ -2,8 +2,10 @@ package com.karrot.karrotmarket;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 
-@SpringBootApplication
+@SpringBootApplication(exclude={DataSourceAutoConfiguration.class})
+
 public class KarrotmarketApplication {
 
 	public static void main(String[] args) {

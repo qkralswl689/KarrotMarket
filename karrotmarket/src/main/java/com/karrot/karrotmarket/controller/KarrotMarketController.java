50d141159ddc4ecd70066450df9f4e0872473112
qkralswl689@gmail.com <qkralswl689@gmail.com>
2022-01-12 23:07:12 +0900

    회원가입 로직3

--- karrotmarket/src/main/java/com/karrot/karrotmarket/controller/KarrotMarketController.java
+++ karrotmarket/src/main/java/com/karrot/karrotmarket/controller/KarrotMarketController.java
@@ -1,9 +1,12 @@
 package com.karrot.karrotmarket.controller;
 
 import com.karrot.karrotmarket.user.dto.UserDto;
+import com.karrot.karrotmarket.user.entity.UserEntity;
 import com.karrot.karrotmarket.user.service.UserServiceImpl;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpSession;
@@ -12,6 +15,8 @@ import javax.servlet.http.HttpSession;
 @RequestMapping("/")
 public class KarrotMarketController {
 
+    @Autowired
+    private UserServiceImpl userService;
 
     @GetMapping
     public String Hello(Model model) {
@@ -27,6 +32,30 @@ public class KarrotMarketController {
 
         return "html/join";
 
+    }
+
+    @RequestMapping(value="/register",method=RequestMethod.GET)
+    public String registration(Model model){
+        model.addAttribute("userForm", new UserEntity());
+        return "register";
+    }
+
+    // 회원가입 처리 후 로그인
+    @RequestMapping(value="/register",method=RequestMethod.POST)
+    public String registration(@ModelAttribute("userForm") UserEntity userForm, BindingResult bindingResult,
+                               Model model ){
+        String password = userForm.getPassword();
+        //userService.register(userForm);
+        //securityService.autologin(userForm.getUsername(),password);
+        return "/redirect:/goToLogin";
+    }
+
+
+    @PostMapping("/register")
+    public String register(UserDto userdto){
+
+        userService.register(userdto);
+        return "/redirect:/goToLogin";
     }
 
     @GetMapping("/goToLogin")
